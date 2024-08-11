31301d7bce9c3937595ec080098e817faa730f65
Aleksey Kuzmin <kboomska@gmail.com>
2024-08-11 14:12:17 +0300

    1. Basics. 1.2 Basic constructions: Values

---
+++ Basics/lesson_1.2_basic_constructions/02_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1+1 =", 1+1) // 1+1 = 2
+
+	fmt.Println("5.0/2.0 =", 5.0/2.0) // 5.0/2.0 = 2.5
+
+	fmt.Println(true && false) // false
+
+	fmt.Println(true || false) // true
+
+	fmt.Println(!true) // false
+
+	fmt.Println("go" + "lang") // golang
+}
